b5674c1e4f3c88a525c7fdaea6a6425472510a47
Nurgeldi Tuwakow <nt.interpretor@gmail.com>
2022-05-11 16:39:48 +0500

    Added router, need install router

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import Header from './components/layout/Header';
 import Contacts from './components/contacts/Contacts';
 import AddContact from './components/contacts/AddContact';
+import About from './components/pages/About';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Provider } from './context';
 
 import './App.css';
@@ -10,11 +12,16 @@ class App extends Component {
   render() {
     return (
       <Provider>
-        <div>
-          <Header />
-          <Contacts />
-          <AddContact />
-        </div>
+        <Router>
+          <div>
+            <Header />
+            <Switch>
+              <Route exact path="/" component={Contacts} />
+              <Route exact path="/contacts/add" component={AddContact} />
+              <Route exact path="/about" component={About} />
+            </Switch>
+          </div>
+        </Router>
       </Provider>
     );
   }
